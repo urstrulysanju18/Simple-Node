7e3f277c3f0d010794adcf26bef1ffce97ce0f7b
urstrulysanju18 <122096639+urstrulysanju18@users.noreply.github.com>
2025-03-25 12:33:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,66 +19,66 @@ app.get('/blogs',(req,res)=>{
     res.send('Blog Post');
 })
 
-app.get('/product',async(req,res)=>{
-    try{
-        const product = await Product.find();
-        res.status(200).json(product);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
-app.get('/product/:id',async(req,res)=>{
-    try{
-        const {id}=req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
+// app.get('/product',async(req,res)=>{
+//     try{
+//         const product = await Product.find();
+//         res.status(200).json(product);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
+// app.get('/product/:id',async(req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
 
-// POST
-app.post('/product',async(req,res)=>{
-    try{
-        const product = await Product.create(req.body);
-        res.status(500).json(product);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
+// // POST
+// app.post('/product',async(req,res)=>{
+//     try{
+//         const product = await Product.create(req.body);
+//         res.status(500).json(product);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
 
-// PUT
-app.put('/product/:id',async(req,res)=>{
-    try{
-        const {id}=req.params;
-        const product = await Product.findByIdAndUpdate(id,req.body)
-        if(!product){
-            return res.status(404).send('Product not found');
-        }
-        res.status(200).json(product);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
+// // PUT
+// app.put('/product/:id',async(req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product = await Product.findByIdAndUpdate(id,req.body)
+//         if(!product){
+//             return res.status(404).send('Product not found');
+//         }
+//         res.status(200).json(product);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
 
-// DELETE
-app.delete('/product/:id',async(req,res)=>{
-    try{
-        const {id}=req.params;
-        const product=await Product.findByIdAndDelete(id);
-        if(!product){
-            res.status(404).send('Product not found');
-        }
-        res.status(200).json(product);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
+// // DELETE
+// app.delete('/product/:id',async(req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product=await Product.findByIdAndDelete(id);
+//         if(!product){
+//             res.status(404).send('Product not found');
+//         }
+//         res.status(200).json(product);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
 
 
 app.listen(8000,()=>{
